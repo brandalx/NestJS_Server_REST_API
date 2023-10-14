14d0626068bbb604e7f2a756fbe53ae304f5c3d3
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-14 18:59:04 +0400

    feat: user module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AtuhModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AtuhModule],
+  imports: [AtuhModule, UserModule],
 })
 export class AppModule {}
