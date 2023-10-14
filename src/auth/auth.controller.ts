10705110b8a8b6e76247bb54d60167027c97ca5d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-14 19:18:38 +0400

    feat: response from signin signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
 
-@Controller({})
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Res() res: Response) {
+    return res.json({ msg: 'This is signup route test' });
+  }
+
+  @Post('signin')
+  signin() {
+    return 'This is signin route test';
+  }
+}
