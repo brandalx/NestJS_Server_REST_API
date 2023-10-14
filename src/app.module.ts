f5c26bd50057422e4c16e30639d7a8ee19be680e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-14 18:59:41 +0400

    feat: bookmark module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AtuhModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AtuhModule, UserModule],
+  imports: [AtuhModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
