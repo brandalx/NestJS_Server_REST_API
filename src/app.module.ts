a540b3d81311971ad0e21efc0db217342df1f955
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-14 20:07:10 +0400

    feat: prisma module, prisma service init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AtuhModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AtuhModule, UserModule, BookmarkModule],
+  imports: [AtuhModule, UserModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
