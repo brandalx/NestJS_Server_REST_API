7475f91eea8b7667ab24920f6e69be824333126a
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-15 15:55:08 +0400

    feat: dto, auth.dto interface

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup() {
+
+  // to directly call Request:
+  // signup(@Req() req: Request) {
+
+  //     else use body decorator
+  signup(@Body() dto: any) {
+    console.log({ dto: dto });
     return this.authService.signup();
   }
   @Post('signin')
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,4 @@
+export interface AuthdTO {
+  email: string;
+  password: string;
+}
