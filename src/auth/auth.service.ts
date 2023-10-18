42b11e799d494c768efc99c165a0e90b955b1b6b
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-18 18:16:27 +0400

    feat: trycatch block for sign up func

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,36 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
 import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDTO) {
-    //gen password hash
-    const hash = await argon2.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hash,
-      },
-      //   select: {
-      //     id: true,
-      //     email: true,
-      //     createdAt: true,
-      //   },
-    });
+    try {
+      //gen password hash
+      const hash = await argon2.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+        //   select: {
+        //     id: true,
+        //     email: true,
+        //     createdAt: true,
+        //   },
+      });
 
-    // todo: write transformer
+      // todo: write transformer
 
-    delete user.hash;
-    //save the new user into db
-    return user;
+      delete user.hash;
+      //save the new user into db
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials error');
+        }
+        throw error;
+      }
+    }
 
     // return the saved user
-    return { msg: 'This is signup route' };
+    // return { msg: 'This is signup route' };
   }
   signin() {
     return { msg: 'This is signin route' };
