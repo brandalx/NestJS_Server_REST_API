b9f9059ef9077034678adfc223ee3310efad8a9d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-19 04:01:25 +0400

    feat: user decorator, code optimization

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+// import { AuthGuard } from '@nestjs/passport';
+// import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
+//moved up to the controller level to all routes of the controller required jwt token guard
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
+  //   getMe(@Req() req: Request) {
+  getMe(@GetUser() user: User) {
     console.log({
-      user: req.user,
+      user: user,
     });
-    return req.user;
+    return user;
   }
 }
