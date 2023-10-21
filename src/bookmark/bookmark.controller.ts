9b4acaaf96282505d1f0d8105cb3116b44ae3e35
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-21 07:22:18 +0400

    feat: Implement CRUD operations in Bookmar cntrl

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -2,12 +2,15 @@ import {
   Controller,
   Delete,
   Get,
+  Param,
+  ParseIntPipe,
   Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard/index';
 import { BookmarkService } from './bookmark.service';
+import { GetUser } from 'src/auth/decorator';
 
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
@@ -15,13 +18,16 @@ export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
 
   @Get()
-  getBookmarks() {}
-  @Get()
-  getBookmarksById() {}
+  getBookmarks(@GetUser('id') userId: number) {}
+  @Get(':id')
+  getBookmarksById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {}
   @Post()
-  createBookmarks() {}
+  createBookmarks(@GetUser('id') userId: number) {}
   @Patch()
-  editBookmarksById() {}
+  editBookmarksById(@GetUser('id') userId: number) {}
   @Delete()
-  deleteBookmarksById() {}
+  deleteBookmarksById(@GetUser('id') userId: number) {}
 }
