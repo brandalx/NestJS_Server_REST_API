424c0b60fdd972bf6ab3fbfe614fff77b3e4fa30
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-23 18:07:57 +0400

    feat: consoling all endpoints available

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,25 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3333);
+
+  const server = app.getHttpServer();
+
+  // console all endpoints
+  const router = server._events.request._router;
+
+  const existingRoutes = router.stack
+    .map((routeObj) => {
+      if (routeObj.route) {
+        return {
+          route: {
+            path: routeObj.route?.path,
+            method: routeObj.route?.stack[0].method,
+          },
+        };
+      }
+    })
+    .filter((item) => item !== undefined);
+
+  console.log(existingRoutes);
 }
 bootstrap();
